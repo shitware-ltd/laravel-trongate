87841bff2374c994096b85693c124bbe60379aa7
Mathias Jorgensen <mathias.jorqensen@gmail.com>
2022-10-04 23:33:23 +0200

    godverdomme

---
+++ src/lang/nl/translation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Je hebt een hele grote fout gemaakt bij het gebruik van Laravel',
+    'subtitle' => 'Gebruik Trongate!',
+    'message' => 'Je wordt doorgestuurd over :countdown seconde(n)',
+];
